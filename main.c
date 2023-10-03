d2ee1a6f249b6ce7dad93631f1a757c6eed6c0cf
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-10-03 07:34:54 -0600

    calculate de log aans validate data

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include<math.h>
 
 int main()
 {
-    printf("Hello World");
+    float num = 0, result = 0;
+    
+    printf("ingresa un dato:\n");
+    scanf("%f", &num);
+
+    if(num > 0){
+        result = num * log(num);
+        printf("el resultado de f(x) = x*log(x) es: %f \n", result);
+    } else {
+        printf("Dato no valido");
+    }
+
+    result = log(num);
 
     return 0;
 }
